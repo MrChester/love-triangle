a9d9a9c4497651598914cec123eb4da4f54f8112
Eugene Romankov <iromankov2010@gmail.com>
2018-02-20 10:39:06 +0300

    Implement getLoveTrianglesCount() function

--- src/index.js
+++ src/index.js
@@ -3,6 +3,32 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
+    let count = 0;
+    let arrLength = preferences.length;
+    let firstSpichonee = 0,
+        firstSpichoneePosition = 0,
+        secondSpichonee = 0,
+        secondSpichoneePosition = 0,
+        thirdSpichonee = 0,
+        thirdSpichoneePosition = 0;
+
+    for (let i = 0; i < arrLength - 1; i++) {
+        firstSpichonee = preferences[i];
+        firstSpichoneePosition = i + 1;
+
+        thirdSpichoneePosition = firstSpichonee;
+        thirdSpichonee = preferences[firstSpichonee - 1];
+
+        secondSpichoneePosition = thirdSpichonee;
+        secondSpichonee = firstSpichoneePosition;
+
+        if ((preferences[secondSpichoneePosition - 1] === secondSpichonee) &&
+            (preferences[thirdSpichoneePosition - 1] === thirdSpichonee)) {
+            count++;
+        }
+
+    }
 
 
+    return count;
 };
