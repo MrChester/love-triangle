8a27ed8ed92fcf8563bd599f0d92e92791d91f4a
Eugene Romankov <iromankov2010@gmail.com>
2018-02-20 12:37:26 +0300

    Fix declaration of variables

--- src/index.js
+++ src/index.js
@@ -5,25 +5,19 @@
 module.exports = function getLoveTrianglesCount(preferences = []) {
     let count = 0;
     let arrLength = preferences.length;
-    let firstSpichonee = 0,
-        firstSpichoneePosition = 0,
-        secondSpichonee = 0,
-        secondSpichoneePosition = 0,
-        thirdSpichonee = 0,
-        thirdSpichoneePosition = 0;
 
-    for (let i = 0; i < arrLength - 1; i++) {
-        firstSpichonee = preferences[i];
-        firstSpichoneePosition = i + 1;
+    for (let i = 0; i < arrLength; i++) {
+        let firstSpichonee = preferences[i];
+        let firstSpichoneePosition = i + 1;
 
-        thirdSpichoneePosition = firstSpichonee;
-        thirdSpichonee = preferences[firstSpichonee - 1];
+        let thirdSpichoneePosition = firstSpichonee;
+        let thirdSpichonee = preferences[firstSpichonee - 1];
 
-        secondSpichoneePosition = thirdSpichonee;
-        secondSpichonee = firstSpichoneePosition;
+        let secondSpichoneePosition = thirdSpichonee;
+        let secondSpichonee = firstSpichoneePosition;
 
-        if ((preferences[secondSpichoneePosition - 1] === secondSpichonee) &&
-            (preferences[thirdSpichoneePosition - 1] === thirdSpichonee)) {
+        if (preferences[secondSpichoneePosition - 1] === secondSpichonee &&
+            preferences[thirdSpichoneePosition - 1] === thirdSpichonee) {
             count++;
         }
 
